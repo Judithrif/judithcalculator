bcc50a0f8bf0448dc305bf6721a87e01ddefbbeb
Judithrif <judithriefd@gmail.com>
2021-06-19 22:22:57 +0700

    judithrifcalculator

---
+++ src/components/Heading.vue
@@ -0,0 +1,19 @@
+<template>
+ <h1>{{message}}</h1>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            message : "Calculator | by JudithRif"
+        }
+    }
+}
+</script>
+
+<style>
+h1{
+    color: #fff;
+}
+</style>
